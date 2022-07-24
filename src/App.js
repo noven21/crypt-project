0fe7b07c37c164624b8747e40b0a0a329fef7b45
noven latigo <77792034+noven21@users.noreply.github.com>
2022-07-24 16:02:40 +0800

    get coins data from api and creating CoinSearch components and CoinItem

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
 	useEffect(() => {
 		axios.get(url).then((response) => {
 			setCoins(response.data);
-			console.log(response.data);
+			// console.log(response.data);
 		});
 	}, [url]);
 
--- src/components/CoinSearch.jsx
+++ src/components/CoinSearch.jsx
@@ -1,7 +1,43 @@
 import React from 'react';
 
-const CoinSearch = () => {
-	return <div>CoinSearch</div>;
+import CoinItem from './CoinItem';
+
+const CoinSearch = ({ coins }) => {
+	console.log(coins);
+	return (
+		<div>
+			<div>
+				<h1>Search Crypto</h1>
+				<form>
+					<input
+						type='text'
+						placeholder='Search a coin'
+					/>
+				</form>
+			</div>
+
+			<table>
+				<thead>
+					<tr>
+						<th></th>
+						<th>#</th>
+						<th>Coin</th>
+						<th></th>
+						<th>Price</th>
+						<th>24h</th>
+						<th>24h Volume</th>
+						<th>Mkt</th>
+						<th>Last 7 Days</th>
+					</tr>
+				</thead>
+				<tbody>
+					{coins.map((coin) => (
+						<CoinItem key={coin.id} coin={coin} />
+					))}
+				</tbody>
+			</table>
+		</div>
+	);
 };
 
 export default CoinSearch;
