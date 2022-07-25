9d4668b456fa302afcb687a202395d3f3f4b4adb
noven latigo <77792034+noven21@users.noreply.github.com>
2022-07-25 13:38:05 +0800

    rendering the Coin Details in CoinPage Component

--- src/App.js
+++ src/App.js
@@ -10,12 +10,13 @@ import SignIn from './routes/SignIn';
 import SignUp from './routes/SignUp';
 import Account from './routes/Account';
 import axios from 'axios';
+import CoinPage from './routes/CoinPage';
 
 function App() {
 	const [coins, setCoins] = useState([]);
 
 	const url =
-		'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true';
+		'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=true';
 
 	useEffect(() => {
 		axios.get(url).then((response) => {
@@ -44,6 +45,12 @@ function App() {
 					path='/account'
 					element={<Account />}
 				/>
+				<Route
+					path='/coin/:coinId'
+					element={<CoinPage />}
+				>
+					<Route path=':coinId' />
+				</Route>
 			</Routes>
 		</ThemeProvider>
 	);
--- src/components/Trending.jsx
+++ src/components/Trending.jsx
@@ -23,8 +23,9 @@ const Trending = () => {
 				Trending Coins
 			</h1>
 			<div className='grid md:grid-cols-2 lg:grid-cols-3 gap-4 '>
-				{trending.map((coin) => (
+				{trending.map((coin, idx) => (
 					<div
+						key={idx}
 						className='rounded-div
                     flex
                     justify-between
